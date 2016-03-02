2cc0987a96fe30d240bc5eb6a9eb80b428f08526
Dr Tim Cooper <tim@skedgo.com>
2016-03-02 16:29:47 +1030

    A tiny bit more testing.

--- Tester/src/Tester.java
+++ Tester/src/Tester.java
@@ -6,7 +6,9 @@ public class Tester {
     @Test
     public void go()
     {
-        System.out.println(TimezoneMapper.latLngToTimezoneString(65.012197, 25.471152));
+        assert TimezoneMapper.latLngToTimezoneString(65.012197, 25.471152).equals("Europe/Helsinki");
+        assert TimezoneMapper.latLngToTimezoneString(41.8788764, -87.6359149).equals("America/Chicago");
+        assert TimezoneMapper.latLngToTimezoneString(42.75676, -0.092723).equals("Europe/Paris");
     }
 
 }
