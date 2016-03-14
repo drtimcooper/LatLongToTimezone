e64c37428a899637e04cc8f3a8552f0355dfe8ff
iamrichardjones <iamrichardjones@gmail.com>
2016-03-14 10:04:44 +0000

    update to test to make it use JUnit Assertions rather than java assertions

--- Tester/src/Tester.java
+++ Tester/src/Tester.java
@@ -1,14 +1,14 @@
 import org.junit.Test;
 
-
+import static org.junit.Assert.assertEquals;
 
 public class Tester {
     @Test
     public void go()
     {
-        Assert.assertEquals("Europe/Helsinki", TimezoneMapper.latLngToTimezoneString(65.012197, 25.471152));
-        Assert.assertEquals("America/Chicago", TimezoneMapper.latLngToTimezoneString(41.8788764, -87.6359149));
-        Assert.assertEquals("Europe/Paris", TimezoneMapper.latLngToTimezoneString(42.75676, -0.092723));
+        assertEquals("Europe/Helsinki", TimezoneMapper.latLngToTimezoneString(65.012197, 25.471152));
+        assertEquals("America/Chicago", TimezoneMapper.latLngToTimezoneString(41.8788764, -87.6359149));
+        assertEquals("Europe/Paris", TimezoneMapper.latLngToTimezoneString(42.75676, -0.092723));
     }
 
 }
