03a383add23345d377a4bdd55a0488751defd345
Jon Andersen <jon@andersen.re>
2016-12-07 10:52:18 -0500

    Added carthage support

---
+++ LatLongToTimezone/LatLongToTimezone.h
@@ -0,0 +1,17 @@
+//
+//  LatLongToTimezone.h
+//  LatLongToTimezone
+//
+//  Created by Jon Andersen on 12/7/16.
+//  Copyright © 2016 Jon Andersen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LatLongToTimezone.
+FOUNDATION_EXPORT double LatLongToTimezoneVersionNumber;
+
+//! Project version string for LatLongToTimezone.
+FOUNDATION_EXPORT const unsigned char LatLongToTimezoneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LatLongToTimezone/PublicHeader.h>
